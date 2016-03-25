c7b14a5c740112d6a635926b231ff82fb2fa9285
Emanuele Biancardi <goffreder@gmail.com>
2016-03-26 00:32:46 +0100

    Added UI testing for App component.

---
+++ test/components/App_spec.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { createRenderer } from 'react-addons-test-utils';
+
+import { expect } from 'chai';
+
+import App from '../../src/components/App';
+
+describe('App', () => {
+    const renderer = createRenderer();
+
+    it('renders correctly with no props and no children', () => {
+        renderer.render(<App />);
+
+        const component = renderer.getRenderOutput();
+
+        expect(component.type).to.equal('div');
+        expect(component.props.children.length).to.equal(4);
+        expect(component.props.children[0].type.displayName)
+            .to.equal('Connect(AppBar)');
+        expect(component.props.children[1].type.displayName)
+            .to.equal('Connect(LeftNav)');
+        expect(component.props.children[2]).to.be.undefined;
+        expect(component.props.children[3]).to.be.null;
+    });
+
+    it('renders children correctly', () => {
+        renderer.render(<App><div /></App>);
+
+        const component = renderer.getRenderOutput();
+
+        expect(component.props.children[2].type).to.equal('div');
+    });
+
+    it('renders the loader if relative prop is passed', () => {
+        renderer.render(<App loading />);
+
+        const component = renderer.getRenderOutput();
+
+        expect(component.props.children[3].type.displayName)
+            .to.equal('Connect(Loader)');
+    });
+});
