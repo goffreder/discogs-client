ccb6037af5957f84a6e03aa4489658fcb0c1f3d5
Emanuele Biancardi <goffreder@gmail.com>
2016-03-28 04:42:50 +0200

    Restored correct collection API endpoint.

--- src/constants/api.js
+++ src/constants/api.js
@@ -2,5 +2,5 @@ export const ROOT = 'https://api.discogs.com';
 
 const base = '/users/goffreder';
 
-export const COLLECTION_ENDPOINT = base + '/collection/folderrrs/0/releases';
+export const COLLECTION_ENDPOINT = base + '/collection/folders/0/releases';
 export const WANTLIST_ENDPOINT = base + '/wants';
