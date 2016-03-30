03191fc01bc0182f4be1b616db9a8caad51b7b39
Emanuele Biancardi <goffreder@gmail.com>
2016-03-30 23:18:17 +0200

    Release component draft.

--- src/components/ReleaseCard.jsx
+++ src/components/ReleaseCard.jsx
@@ -3,39 +3,60 @@ import React from 'react';
 import { shouldComponentUpdate } from 'react-addons-pure-render-mixin';
 
 import Card from 'material-ui/lib/card/card';
-import CardMedia from 'material-ui/lib/card/card-media';
 import CardTitle from 'material-ui/lib/card/card-title';
-import CardText from 'material-ui/lib/card/card-text';
+import CardMedia from 'material-ui/lib/card/card-media';
 
-export default class CardExampleWithAvatar extends React.Component {
+import ReleaseTracklist from '../containers/ReleaseTracklist';
+
+import { DEFAULT_THUMB } from '../constants/constants';
+
+export default class ReleaseCard extends React.Component {
     constructor(props) {
         super(props);
 
         this.shouldComponentUpdate = shouldComponentUpdate.bind(this);
     }
 
+    static propTypes = {
+        artist: React.PropTypes.string,
+        title: React.PropTypes.string,
+        year: React.PropTypes.number,
+        tracklist: React.PropTypes.arrayOf(React.PropTypes.shape({
+            position: React.PropTypes.string,
+            duration: React.PropTypes.string,
+            title: React.PropTypes.string
+        }))
+    }
+
+    static defaultProps = {
+        tracklist: []
+    }
+
     render() {
+        const tracklist = this.props.tracklist.length
+            ? <ReleaseTracklist tracklist={this.props.tracklist} />
+            : null;
+
         return (
             <Card style={{
-                width: 600
+                width: 400
             }}>
                 <CardMedia
                     overlay={
                         <CardTitle
-                            title="Overlay title"
-                            subtitle="Overlay subtitle"
+                            title={`${this.props.title} - ${this.props.artist}`}
+                            subtitle={this.props.year}
                         />
                     }>
-                    <img src="http://lorempixel.com/600/337/nature/"/>
+                    <div style={{
+                        height: 200,
+                        backgroundImage: `url(${DEFAULT_THUMB})`,
+                        backgroundPositionY: '50%',
+                        backgroundSize: 'cover'
+                    }} />
                 </CardMedia>
-                <CardTitle title="Tracklist" subtitle="Card subtitle"/>
-                <CardText>
-                    {'Donec mattis pretium massa. Aliquam erat volutpat. '}
-                    {'mattis quis lacus id, pellentesque lobortis odio.'}
-                </CardText>
+                {tracklist}
             </Card>
         );
     }
 }
-
-export default CardExampleWithAvatar;
